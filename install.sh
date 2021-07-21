caf6fec71d7885607c93b6ddf99a1a2bcf39ffc8
Abdullah Khawaja <hax.3xploit@gmail.com>
2021-07-21 10:02:58 +0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -47,4 +47,4 @@ wget https://raw.githubusercontent.com/hax3xploit/EZ-Tmux/master/tmux.conf -O .t
 sleep 1s
 tmux source ~/.tmux.conf
 sleep 2s
-tmux new -d -s mySession;tmux send-keys -t  mySession.0 clear ENTER 'echo  "1 - Press prefix + I (capital i, as in Install - e.g: ctrl+a i) to install the plugins.\n2 - type tmux source ~/.tmux.conf and create a new session"' ENTER;tmux a -t mySession
+tmux new -d -s mySession;tmux send-keys -t  mySession.0 clear ENTER 'echo  "Press prefix + I (capital i, as in Install - e.g: ctrl+a i) to install the plugins."' ENTER;tmux a -t mySession
